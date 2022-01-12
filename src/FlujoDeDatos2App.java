74df4390c6bf4681076fadcc3a17e3bc939f047e
sillyck <ribellas2002@gmail.com>
2022-01-12 09:55:13 +0100

    act2

---
+++ src/FlujoDeDatos2App.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+public class FlujoDeDatos2App {
+
+	public static void main(String[] args) {
+		
+		String nombre = JOptionPane.showInputDialog("Introduce tu nombre");
+		
+		JOptionPane.showMessageDialog(null, "Tu nombre es " + nombre);
+
+	}
+
+}
