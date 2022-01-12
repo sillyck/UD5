0f6daab02909fd25d2c0ec282479e0ca4d8a39ef
sillyck <ribellas2002@gmail.com>
2022-01-12 09:47:39 +0100

    act1

---
+++ src/FlujoDeDatos1App.java
@@ -0,0 +1,19 @@
+
+public class FlujoDeDatos1App {
+
+	public static void main(String[] args) {
+		
+		int n1 = 20;
+		int n2 = 10;
+		
+		if(n1 > n2) {
+			System.out.println("El primer numero es mas grade que el segundo");
+		}else if(n2 > n1) {
+			System.out.println("El segundo numero es mas grade que el primero");
+		}else {
+			System.out.println("Los numeros son iguales");
+		}
+		
+	}
+
+}
